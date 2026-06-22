15f34760a2a48adb34872048f2f48f5a71a9bf7e
ravitejakPixenTech <ravitejak@pixentech.com>
2026-06-23 01:39:44 +0530

    Fix recharts webpack resolution issues by adding dependencies and transpilePackages

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  transpilePackages: ['recharts', 'react-smooth', 'react-transition-group'],
   images: {
     remotePatterns: [
       {
